9571db6c4baad48e759bd78b8d9bd66883635150
unknown <g9179@.fwuk.fwc.com>
2010-09-17 10:39:37 +0100

    Wrap anon Actions & Func delegates to create LuaFunctions

--- luainterface/src/LuaInterface/Extensions/LuaExtensions.cs
+++ luainterface/src/LuaInterface/Extensions/LuaExtensions.cs
@@ -20,5 +20,16 @@ namespace LuaInterface.Extension
 			}
 
 		}
+		
+		public static LuaFunction Action(this Lua lua, Action fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Action<T1, T2>(this Lua lua, Action<T1, T2> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Action<T1, T2, T3>(this Lua lua, Action<T1, T2, T3> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Action<T1, T2, T3, T4>(this Lua lua, Action<T1, T2, T3, T4> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		
+		public static LuaFunction Func<TResult>(this Lua lua, Func<TResult> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Func<T, TResult>(this Lua lua, Func<T, TResult> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Func<T1, T2, TResult>(this Lua lua, Func<T1, T2, TResult> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Func<T1, T2, T3, TResult>(this Lua lua, Func<T1, T2, T3, TResult> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
+		public static LuaFunction Func<T1, T2, T3, T4, TResult>(this Lua lua, Func<T1, T2, T3, T4, TResult> fun) { return lua.LambdaFunction(fun.Target, fun.Method); }
 	}
 }
